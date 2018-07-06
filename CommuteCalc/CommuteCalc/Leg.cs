fe543c6beb9b9aa77f23c859c4dc034f640538d8
Nina <nfernandes392@users.noreply.github.com>
2018-07-06 00:25:20 -0700

    Initial commit

---
+++ CommuteCalc/CommuteCalc/Leg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommuteElements
+{
+    public class Leg
+    {
+        public enum Node
+        {
+            Home,
+            Stop40,
+            Stop90,
+            Coliseum,
+            BayFair,
+            DublinPleasanton,
+            Stop14,
+            Stop30R,
+            Work
+        }
+        public enum Transport
+        {
+            Walk,
+            Bike,
+            Bus,
+            Bart
+        }
+        public Leg(Node pointA, Node pointB, Transport transportMethod, int mins)
+        {
+            PointA = pointA;
+            PointB = pointB;
+            TransportMethod = transportMethod;
+            TravelTime = mins;
+        }
+
+        public Node PointA { get; private set; }
+
+        public Node PointB { get; private set; }
+
+        public Transport TransportMethod { get; private set; }
+
+        public int TravelTime { get; private set; }
+    }
+}
---
+++ CommuteCalc/CommuteCalc/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace CommuteCalc
+{
+    using System.Collections.Generic;
+    using CommuteElements;
+    using static CommuteElements.Leg;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // All Paths
+            List<Leg> paths = new List<Leg>();
+
+            // From Home
+            paths.Add(new Leg(Node.Home, Node.Stop90, Transport.Walk, 6));
+            paths.Add(new Leg(Node.Home, Node.Stop90, Transport.Bike, 4));
+            paths.Add(new Leg(Node.Home, Node.Coliseum, Transport.Bike, 17));
+            paths.Add(new Leg(Node.Home, Node.Stop40, Transport.Walk, 3));
+
+            // From Oakland Bus Stop
+            paths.Add(new Leg(Node.Stop90, Node.Coliseum, Transport.Bus, 11));
+            paths.Add(new Leg(Node.Stop40, Node.BayFair, Transport.Bus, 43));
+
+            // From BART
+            paths.Add(new Leg(Node.Coliseum, Node.DublinPleasanton, Transport.Bart, 26));
+            paths.Add(new Leg(Node.BayFair, Node.DublinPleasanton, Transport.Bart, 17));
+
+            // From Dublin/Pleasanton
+            paths.Add(new Leg(Node.DublinPleasanton, Node.Stop14, Transport.Bus, 24));
+            paths.Add(new Leg(Node.DublinPleasanton, Node.Stop30R, Transport.Bus, 40));
+            paths.Add(new Leg(Node.DublinPleasanton, Node.Work, Transport.Bike, 40));
+
+            // From Livermore Bus Stop
+            paths.Add(new Leg(Node.Stop14, Node.Work, Transport.Walk, 18));
+            paths.Add(new Leg(Node.Stop14, Node.Work, Transport.Bike, 8));
+            paths.Add(new Leg(Node.Stop30R, Node.Work, Transport.Walk, 22));
+            paths.Add(new Leg(Node.Stop30R, Node.Work, Transport.Bike, 10));
+
+            Node place = Node.Home;
+            Node end = Node.Work;
+
+            while (place != end)
+            {
+            }
+        }
+    }
+}
